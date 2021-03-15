f57acf49ba4499deb429ec180385310fe9cdd7c9
Momchil Kolev <momchil.kolev@protonmail.com>
2021-03-15 08:55:53 +0200

    ADD Verifier description in 2_deploy_contracts

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,9 +2,9 @@
 // var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var Verifier = artifacts.require("./Verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-var ERC721Mintable = artifacts.require("./CustomERC721Token.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(SquareVerifier);
   deployer.deploy(SolnSquareVerifier);
+  // deployer.deploy(Verifier); // comment in to test TestSquareVerifier
 };
